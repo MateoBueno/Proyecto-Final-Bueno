d1f8adc22b506a47af045deca5eb67c05fcffb2c
Mateo Bueno <121068073+MateoBueno@users.noreply.github.com>
2023-01-27 11:06:02 -0300

    arreglos en el formulario noticia

--- myblog/pagina_blog/forms.py
+++ myblog/pagina_blog/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class NoticiaFormulario(forms.Form):
     titulo = forms.CharField(max_length=256)
     subtitulo = forms.CharField(max_length=1000)
-    cuerpo = forms.CharField(max_length=1000)
+    cuerpo = forms.CharField(max_length=1000, widget=forms.Textarea())
     fecha_publicacion = forms.DateField(required=True)
     autor = forms.CharField(max_length=64)
   
